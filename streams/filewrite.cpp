50cba74dd29b92f576bd22965f7e533fbaee2013
Yagodkin Anatoliy <yatolik@yandex.ru>
2020-01-14 16:08:22 +0300

    Update filewrite.cpp

--- streams/filewrite.cpp
+++ streams/filewrite.cpp
@@ -8,11 +8,11 @@ FileWrite::FileWrite()
 void FileWrite::WriteFromClass(int flag, QString data)
 {
 #if defined (Q_OS_ANDROID)
-    QString path = "/storage/emulated/0/";      //Android
+        QString path = "/storage/emulated/0/";      //Android
 #elif defined (Q_OS_WIN)
-    QString path = "C:\\Users\\qt\\";        //Win
+        QString path = "C:\\Users\\qt\\";           //Win
 #else
-    QString path = "/var/log/s500/";            //linux-RPi
+        QString path = "/var/log/s500/";            //linux-RPi
 #endif
     QString name;
     switch(flag)
@@ -20,31 +20,24 @@ void FileWrite::WriteFromClass(int flag, QString data)
     case 0:
         name = "main.txt";
         break;
-
     case 1:
         name = "bluetoothserver.txt";
         break;
-
     case 2:
         name = "bluetoothclient.txt";
         break;
-
     case 3:
         name = "bluetoothchat.txt";
         break;
-
     case 4:
         name = "bluetoothdiscovery.txt";
         break;
-
     case 5:
         name = "mavlink.txt";
         break;
-
     default:
         break;
     }
-
 
     QFile file(path + QDateTime::currentDateTime().toString("dd.MM.yyyy") + "_" + name);
     if (!file.open(QIODevice::Append))
