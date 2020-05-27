06157b100b2ce70ae00bb0b445ab7cb851294662
reve1 <yatolik@yandex.ru>
2020-05-27 09:07:31 +0300

    0.26.0

--- main.cpp
+++ main.cpp
@@ -36,19 +36,31 @@ int main(int argc, char *argv[])
     Vehicle *vh = new Vehicle;
     vh->moveToThread(VhThred);
     VhThred->start();
-    QObject::connect(vh, SIGNAL(LocalVehiclePositionInfo(unsigned long,double,double,float,float)), md, SLOT(setLocalVehiclePositionInfo(unsigned long,double,double,float,float)));
-    QObject::connect(vh, SIGNAL(LocalVehicleGPSInfo(unsigned long,int,int)), md, SLOT (setLocalVehicleGPSInfo(unsigned long,int,int)));
-    QObject::connect(vh, SIGNAL(LocalVehicleBatteryInfo(unsigned long,float)), md, SLOT (setLocalVehicleBatteryInfo(unsigned long,float)));
-    QObject::connect(vh, SIGNAL(LocalVehicleAngle(unsigned long,float)), md, SLOT (setLocalVehicleAngle(unsigned long,float)));
-    QObject::connect(vh, SIGNAL(LocalVehicleFlightMode(unsigned long,int)), md, SLOT (setLocalVehicleFlightMode(unsigned long,int)));
-    QObject::connect(md, SIGNAL(sendLocalVehicleInfo(unsigned long, double, double,float,float,int,int,float,int,int,int,float,int)), client, SLOT(sendLocalVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)));
+    QObject::connect(vh, SIGNAL(LocalVehiclePositionInfo(unsigned long,double,double,float,float)),
+                     md, SLOT(setLocalVehiclePositionInfo(unsigned long,double,double,float,float)));
+    QObject::connect(vh, SIGNAL(LocalVehicleGPSInfo(unsigned long,int,int)),
+                     md, SLOT (setLocalVehicleGPSInfo(unsigned long,int,int)));
+    QObject::connect(vh, SIGNAL(LocalVehicleBatteryInfo(unsigned long,float)),
+                     md, SLOT (setLocalVehicleBatteryInfo(unsigned long,float)));
+    QObject::connect(vh, SIGNAL(LocalVehicleAngle(unsigned long,float)),
+                     md, SLOT (setLocalVehicleAngle(unsigned long,float)));
+    QObject::connect(vh, SIGNAL(LocalVehicleFlightMode(unsigned long,int)),
+                     md, SLOT (setLocalVehicleFlightMode(unsigned long,int)));
+    QObject::connect(md, SIGNAL(sendLocalVehicleInfo(unsigned long, double, double,float,float,int,int,float,int,int,int,float,int)),
+                     client, SLOT(sendLocalVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)));
     //QObject::connect(vh, SIGNAL(LocalVehicleInfo(unsigned long,double,double,float,float,int,int)), client, SLOT(sendLocalVehicleInfo(unsigned long,double,double,float,float,int,int)));
-    QObject::connect(server, SIGNAL(ReceivedRemoteVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)), md, SLOT(setRemoteVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)));
-    QObject::connect(md, SIGNAL(goToPosition(double,double,float,float)), vh, SLOT(fly(double,double,float,float)),Qt::DirectConnection);
-    QObject::connect(md, SIGNAL(Takeoff()), vh, SLOT(setTakeOff()),Qt::DirectConnection);
-    QObject::connect(md, SIGNAL(ReturnToLaunch()), vh, SLOT(setReturnToLaunch()),Qt::DirectConnection);
-    QObject::connect(md, SIGNAL(Land()), vh, SLOT(setLand()),Qt::DirectConnection);
-    QObject::connect(VhThred, SIGNAL(started()), vh, SLOT(Run()));                            
+    QObject::connect(server, SIGNAL(ReceivedRemoteVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)),
+                     md, SLOT(setRemoteVehicleInfo(unsigned long,double,double,float,float,int,int,float,int,int,int,float,int)));
+    QObject::connect(md, SIGNAL(goToPosition(double,double,float,float)),
+                     vh, SLOT(fly(double,double,float,float)),Qt::DirectConnection);
+    QObject::connect(md, SIGNAL(Takeoff()),
+                     vh, SLOT(setTakeOff()),Qt::DirectConnection);
+    QObject::connect(md, SIGNAL(ReturnToLaunch()),
+                     vh, SLOT(setReturnToLaunch()),Qt::DirectConnection);
+    QObject::connect(md, SIGNAL(Land()),
+                     vh, SLOT(setLand()),Qt::DirectConnection);
+    QObject::connect(VhThred, SIGNAL(started()),
+                     vh, SLOT(Run()));
 #endif
 
     //BluetoothDiscovery *bd = new BluetoothDiscovery;
